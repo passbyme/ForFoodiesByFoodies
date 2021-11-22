ff25d9c83b5c17ae7e8e1208cda0d6175eed3fe8
Antonc92 <anton.constantin.titi@gmail.com>
2021-11-22 19:58:30 +0000

    Baza proiectului

---
+++ app/src/main/java/com/example/ffbf/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ffbf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
